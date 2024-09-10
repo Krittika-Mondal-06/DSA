37262f809d1ee0c463a091381405c5d265414f4d
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2024-09-10 17:16:23 +0530

    Another Code

---
+++ LabAssignment1/Reverse_Array.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the array elements: \n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Reversed array: ");
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
