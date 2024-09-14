4179d4dc53f9b49c8c3a24862e82f84662d97109
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2024-09-14 22:20:58 +0530

    Another Code

---
+++ LabAssignment2/Malloc.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() 
+{
+    int n, i;
+    int *ptr;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    ptr = (int *)malloc(n * sizeof(int));
+    if (ptr == NULL) 
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &ptr[i]);
+    }
+    printf("The entered elements are:\n");
+    for (i = 0; i < n; i++) 
+    {
+        printf("%d ", ptr[i]);
+    }
+    printf("\n");
+    free(ptr);
+    return 0;
+}
